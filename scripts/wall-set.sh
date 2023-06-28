2b3d62d450ece86c446505807b17122b6806bdb4
Adam <shreyas.as.tech@protonmail.com>
2023-06-28 15:11:15 +0530

    Replacing comparison string in wall-set.sg (nireogen -> --set-zoom-fill. Coz otherwise it would replace the floating window properties also)

--- scripts/wall-set.sh
+++ scripts/wall-set.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i "/nitrogen/ c\exec \t\t--no-startup-id nitrogen --restore &" ~/.config/i3/config
+sed -i "/--set-zoom-fill/ c\exec \t\t--no-startup-id nitrogen --restore &" ~/.config/i3/config
 sed -i "/wall/ c\\" ~/.config/i3/config
 rm ~/wall-set.sh
