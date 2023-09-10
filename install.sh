a4a9e1842734dd692a955acc48aa5c8cbe2d58e3
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-10 21:43:36 +0530

    Remove stterm from apt install

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ debianversion=$(cat /etc/debian_version) && debianversion=${debianversion%.*}
 echo ""; echo "Doing a system update & Installing the required programs..."
 apt update && apt upgrade -y
 apt install x11-utils x11-xserver-utils xserver-xorg-video-intel xserver-xorg-input-libinput -y
-apt install i3 i3status lightdm slick-greeter xfce4-terminal stterm dmenu picom -y
+apt install i3 i3status lightdm slick-greeter xfce4-terminal dmenu picom -y
 apt install fonts-powerline curl imagemagick pulseaudio pavucontrol wget nitrogen python3-pip fonts-font-awesome -y
 
 # Change the current working directory
