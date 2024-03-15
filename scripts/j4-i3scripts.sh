776a66038c29efb2c28ab8fc081e4c0728cab096
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-03-15 16:04:23 +0530

    fix: Use apt instead of pip to install i3ipc

--- scripts/j4-i3scripts.sh
+++ scripts/j4-i3scripts.sh
@@ -2,7 +2,7 @@
 
 echo ""; echo "Installing j4-dmenu-desktop and dependencies of i3scripts..."
 if [ "$debianversion" -lt 12 ]; then
-	cp programs/j4-dmenu-desktop /usr/local/bin/ && pip install i3ipc fontawesome
+	cp programs/j4-dmenu-desktop /usr/local/bin/ && sudo apt-get install python3-i3ipc && pip install fontawesome
 else
-	apt install j4-dmenu-desktop -y && pip install i3ipc fontawesome --break-system-packages # don't worry, it won't break anything
+	apt install j4-dmenu-desktop -y && sudo apt-get install python3-i3ipc  && pip install fontawesome --break-system-packages # don't worry, it won't break anything
 fi
